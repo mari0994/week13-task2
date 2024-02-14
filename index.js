fcae760ed1c48281497eb822057847eb59bf81c8
mari0994 <mbabiy@mail.ru>
2024-02-14 22:01:41 +0300

    update code

--- index.js
+++ index.js
@@ -19,10 +19,46 @@ function calculateDaysUntilBirthday (){
             COMMENT.innerText = `До вашего дня рождения остался ${daysUntil} день.`;
         } else if (daysUntil % 10 === 2 || daysUntil % 10 === 3 || daysUntil % 10 === 4){
             COMMENT.innerText = `До вашего дня рождения осталось ${daysUntil} дня.`;
+        } else if(daysUntil<=-1) {
+            COMMENT.textContent = `Ваш день рождения уже прошел.`
+        } else if(daysUntil ===0){
+            COMMENT.textContent = `Ваш день рождения сегодня.`
         } else {
             COMMENT.innerText = `До вашего дня рождения осталось ${daysUntil} дней.`;
         }
     }
 }
+
+
+// Оптимизированный вариант !
+
+// function calculateDaysUntilBirthday (){
+//     if(INPUT.value === ''){
+//         COMMENT.innerText = 'Введите дату';
+//         COMMENT.style.color = 'red';
+//     } else {
+//         const birthdayDate = INPUT.value;
+//         const timeDifference = Date.parse(birthdayDate) - Date.now();
+//         const daysUntil = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+//         COMMENT.style.color = 'black';
+
+//         const lastDigit = daysUntil % 10;
+//         const lastTwoDigits = daysUntil % 100;
+//         if (lastTwoDigits >= 11 && lastTwoDigits <= 14) {
+//             COMMENT.innerText = `До вашего дня рождения осталось ${daysUntil} дней.`;
+//         }
+//         else if (lastDigit === 1){
+//             COMMENT.innerText = `До вашего дня рождения остался ${daysUntil} день.`;
+//         } else if (lastDigit >=2 && lastDigit <= 4){
+//             COMMENT.innerText = `До вашего дня рождения осталось ${daysUntil} дня.`;
+//         } else if(daysUntil<=-1) {
+//             COMMENT.textContent = `Ваш день рождения уже прошел.`
+//         } else if(daysUntil ===0){
+//             COMMENT.textContent = `Ваш день рождения сегодня.`
+//         } else {
+//             COMMENT.innerText = `До вашего дня рождения осталось ${daysUntil} дней.`;
+//         }
+//     }
+// }
 
 BUTTON.addEventListener('click', calculateDaysUntilBirthday);
